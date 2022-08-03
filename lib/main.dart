d98040fd527904209229595a9acdec51cde8c1f7
in-ch <sxin2949@naver.com>
2022-08-03 22:39:19 +0900

    :tada: first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Hello world",
+      style: TextStyle(color: Colors.blue),
+    );
+  }
+}
