00c6feb698b98a4bf4e982e5a20d34cb5b699171
in-ch <sxin2949@naver.com>
2022-08-05 00:08:29 +0900

    :sparkles: #27 메시지보내기

---
+++ lib/chatting/chat/message.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('time', descending: true)
+          .snapshots(),
+      builder: (BuildContext context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = snapshot.data!.docs;
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                chatDocs[index]['text'],
+                style: const TextStyle(fontSize: 20.0),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: library_private_types_in_public_api, avoid_print
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:signup_project/chatting/chat/message.dart';
+import 'package:signup_project/chatting/chat/new_message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -50,31 +51,9 @@ class _ChatScreenState extends State<ChatScreen> {
             )
           ],
         ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('chats/wlHWnrdYYtoXcEOPtrFU/message')
-              .snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final docs = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: docs.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    docs[index]['text'],
-                    style: const TextStyle(fontSize: 20.0),
-                  ),
-                );
-              },
-            );
-          },
-        ));
+        body: Container(
+            child: Column(
+          children: const [Expanded(child: Messages()), NewMessage()],
+        )));
   }
 }
